60345372a4e2c33bd8f3cc2b4848b46a7327ac16
lengway <lengway@inbox.ru>
2025-03-27 10:35:18 +0500

    third commit

--- src/findFactorial.java
+++ src/findFactorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class primeOrComposite {
+public class findFactorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -9,32 +9,28 @@ public class primeOrComposite {
         sc.close();
 
         double startTime = System.nanoTime();
-        boolean result = isPrime(number);
+        int result = factorial(number);
         double endTime = System.nanoTime();
 
         double duration = (endTime - startTime) / 1_000_000; // Convert nanoseconds to milliseconds
-        String res = (result ? "Prime " : "Not Prime ");
-        System.out.println("Number " + number + " is " + res);
+        System.out.println("Factorial of  " + number + " is " + result);
         System.out.println("Time taken: " + duration + " milliseconds");
 
         sc.close();
     }
 
     /**
-     * This method finds is given number prime.
+     * This method finds the factorial of the given number.
      * Time complexity: O(n), since it iterates through all elements once.
      *
      * @param number The input number.
-     * @return The boolean answer.
+     * @return The factorial of the number.
      */
-    public static boolean isPrime(int number) {
-        int[] arr = new int[number];
-
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
+    public static int factorial(int number) {
+        if (number == 1) {
+            return 1;
         }
-        return true;
+
+        return factorial(number - 1) * number;
     }
 }
