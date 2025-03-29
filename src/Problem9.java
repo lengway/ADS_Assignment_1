b1fa29ba8f1333f3be12a726c5a3b83e10c1d9fe
lengway <lengway@inbox.ru>
2025-03-29 09:03:53 +0500

    8th commit

---
+++ src/Problem9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.close();
+
+        double startTime = System.nanoTime();
+        int res = binomialCoefficient(n, k);
+        double endTime = System.nanoTime();
+
+        double duration = (endTime - startTime) / 1_000_000; // Convert nanoseconds to milliseconds
+        System.out.println("Binomial coefficient is " + res);
+        System.out.println("Time taken: " + duration + " milliseconds");
+    }
+
+    /**
+     * This method finds a binomial coefficient using recursion and formula C(n, k) = C(n-1, k-1) + C(k, n-1)
+     * Time complexity: O(n), since it calls function once with every single call.
+     *
+     * @param n The n parameter.
+     * @param k The k parameter.
+     * @return The binomial coefficient.
+     */
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+
+}
